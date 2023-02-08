f611ba8f51469c8b0c26c58620011e535ca641cd
Kieran Bailey <k.d.bailey88@gmail.com>
2023-02-07 20:41:32 -0600

    Finished bonus and tidied up the code

--- scripts/da_jobs.sql
+++ scripts/da_jobs.sql
@@ -42,7 +42,7 @@ GROUP BY state;
 
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 SELECT COUNT(DISTINCT title)
-FROM data_analyst_jobs
+FROM data_analyst_jobs;
 -- 881
 
 -- 8.	How many unique job titles are there for California companies?
@@ -71,7 +71,7 @@ ORDER BY star_rating DESC;
 SELECT COUNT(title)
 FROM data_analyst_jobs
 WHERE title LIKE '%_nalyst%'
-	OR title LIKE '%_NALYST%'
+	OR title LIKE '%_NALYST%';
 -- 1669
 
 -- 12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
@@ -80,14 +80,44 @@ FROM data_analyst_jobs
 WHERE title NOT LIKE '%_nalyst%'
 	AND title NOT LIKE '%_nalytics%'
 	AND title NOT LIKE '%_NALYST%'
-	AND title NOT LIKE '%_NALYTICS%'
---4 The non-analytics listings use words like "Data Consultant" or "Data Specialist" or specifically mention Tableau.
+	AND title NOT LIKE '%_NALYTICS%';
+--4. The non-analytics listings use words like "Data Consultant" or "Data Specialist" or specifically mention Tableau.
 
 
 
 
 -- **BONUS:**
+
 -- You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
+SELECT domain, COUNT(title) AS jobs_available
+FROM data_analyst_jobs
+WHERE skill='SQL'
+	AND days_since_posting>21
+GROUP BY domain;
+-- 15 total jobs
+
 --  - Disregard any postings where the domain is NULL. 
---  - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
+SELECT domain, COUNT(title) AS jobs_available
+FROM data_analyst_jobs
+WHERE skill='SQL'
+	AND days_since_posting>21
+	AND domain IS NOT NULL
+GROUP BY domain; 
+-- 11 total jobs
+
+--  - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top.
+SELECT domain, COUNT(title) AS jobs_available
+FROM data_analyst_jobs
+WHERE skill='SQL'
+	AND days_since_posting>21
+	AND domain IS NOT NULL
+GROUP BY domain
+ORDER BY jobs_available DESC;
+--got it
+
 --   - Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
+-- Consulting & Business Services - 5 jobs
+-- Consumer Goods & Services - 2 jobs
+-- Computers & Electronics - 1 jobs
+-- Internet & Software - 1 jobs
+
