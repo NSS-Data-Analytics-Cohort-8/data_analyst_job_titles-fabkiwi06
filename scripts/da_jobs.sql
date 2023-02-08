a3473e977cb5ed90cbc5ad088d35a2bf40db7f19
Kieran Bailey <k.d.bailey88@gmail.com>
2023-02-07 19:27:55 -0600

    # 7 & 8

--- scripts/da_jobs.sql
+++ scripts/da_jobs.sql
@@ -25,24 +25,31 @@ GROUP BY location;
 SELECT COUNT(star_rating)
 FROM data_analyst_jobs
 WHERE location='TN'
-AND star_rating>4
+AND star_rating>4;
 -- 3
 
 -- 5.	How many postings in the dataset have a review count between 500 and 1000?
 SELECT COUNT(review_count)
 FROM data_analyst_jobs
-WHERE review_count BETWEEN 500 AND 1000
+WHERE review_count BETWEEN 500 AND 1000;
 --151
 
 -- 6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
 SELECT location AS "state", AVG(star_rating) AS "avg_rating"
 FROM data_analyst_jobs
-GROUP BY state
+GROUP BY state;
 -- Kansas shows the highest average rating
 
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs
+-- 881
 
 -- 8.	How many unique job titles are there for California companies?
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs
+WHERE location='CA';
+--230
 
 -- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
