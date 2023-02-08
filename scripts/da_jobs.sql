a472da41ba6a96e5f6d39167d0b850920a946618
Kieran Bailey <k.d.bailey88@gmail.com>
2023-02-07 19:52:32 -0600

    Q11

--- scripts/da_jobs.sql
+++ scripts/da_jobs.sql
@@ -68,6 +68,10 @@ ORDER BY star_rating DESC;
 -- American Express with a 4.199 rating
 
 -- 11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
+SELECT COUNT(title)
+FROM data_analyst_jobs
+WHERE title LIKE 'Analyst_%'
+-- 12
 
 -- 12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
 
